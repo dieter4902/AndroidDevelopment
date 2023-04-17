ca83f45a49d87755ea86a86169021ce79e8ed681
adrian <adrianjost1@gmx.de>
2023-04-17 19:36:48 +0200

    zahl buttons funktionieren jetzt

--- app/uebung1/aufgabe4/src/main/java/com/example/aufgabe4/MainActivity.java
+++ app/uebung1/aufgabe4/src/main/java/com/example/aufgabe4/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.aufgabe4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView formula = (TextView) findViewById(R.id.textView_formula);
+        TextView result = (TextView) findViewById(R.id.textView_result);
+
+
+        for (int i = 0; i < 10; i++) {//https://stackoverflow.com/questions/22639218/how-to-get-all-buttons-ids-in-one-time-on-android
+            @SuppressLint("DiscouragedApi") int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+            ((Button) findViewById(id)).setOnClickListener(v -> formula.append(((TextView) v).getText().toString()));
+        }
+
+
     }
 }
