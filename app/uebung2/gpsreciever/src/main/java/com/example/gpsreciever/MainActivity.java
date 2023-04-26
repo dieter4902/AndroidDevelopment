6557fb0e8a0ce14694b389fec3f449107675fb24
adrian <adrianjost1@gmx.de>
2023-04-26 17:40:02 +0200

    optimierung von gps

--- app/uebung2/gpsreciever/src/main/java/com/example/gpsreciever/MainActivity.java
+++ app/uebung2/gpsreciever/src/main/java/com/example/gpsreciever/MainActivity.java
@@ -5,72 +5,91 @@ import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView gpsView;
-    LocationManager locationManager;
 
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Spinner providerSpinner;
+    private String providerString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gpsView = findViewById(R.id.gps_info);
-        requestPermissions();
+        providerString = LocationManager.NETWORK_PROVIDER;
+        initializeLocationTracking();
+        providerSpinner = findViewById(R.id.provider_spinner);
+        providerSpinner.setAdapter(ArrayAdapter.createFromResource(this, R.array.providers, android.R.layout.simple_spinner_item));
+        providerSpinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
 
 
     }
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // permission was granted, yay!
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
-                @SuppressLint("MissingPermission")
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    updateLocation(location);
-                }
-            });
-        } else {
-            // permission denied, boo! Disable the
-            // functionality that depends on this permission.
+    public class MyOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            providerString = ((TextView) view).getText().toString();
+            //Log.d("changed provider", providerString);
+            initializeLocationTracking();
         }
 
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
+
+
+    public void initializeLocationTracking() {
+        // Acquire a reference to the system Location Manager
+        try {
+            locationManager.removeUpdates(locationListener);
+        } catch (NullPointerException ignored) {
+        }
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Register the listener with the Location Manager to receive location updates
+        // Acquire a reference to the system Location Manager
+        // Define a listener that responds to location updates
+        locationListener = location -> {
+            TextView latLng = findViewById(R.id.textLatLng);
+            latLng.setText(location.getLatitude() + " " + location.getLongitude());
+            TextView altitude = findViewById(R.id.textAltitude);
+            altitude.setText(Double.toString(location.getAltitude()));
+            //Log.d("current gps provider", location.getProvider());
+        };
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] perms = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, perms, 42);
+        }
+        locationManager.requestLocationUpdates(providerString, 0L, (float) 0, locationListener);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-    }
-
-    private void requestPermissions() {
-        String[] perms = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION};
-        ActivityCompat.requestPermissions(this, perms, 42);
-        //if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        //ActivityCompat.requestPermissions(this, perms, 42);
-        //}
-    }
-
-
-    private void updateLocation(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-
-        Log.d("test2", "test");
-        Log.d("updated lat", String.valueOf(latitude));
-        Log.d("updated lng", String.valueOf(longitude));
-        gpsView.setText(location.toString());
+        //TODO handle if permission denied
+        //quit("required permissions were not found");
     }
 }
